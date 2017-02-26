c245fbc1929b11e7832d4ebf7f3e4c95730d1096
Evan <huwen126@126.com>
2017-02-26 18:31:44 +0800

    1.0.0_release

---
+++ SearchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchDemo
+//
+//  Created by Azzan on 16/9/26.
+//  Copyright © 2016年 Azzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchDemo
+//
+//  Created by Azzan on 16/9/26.
+//  Copyright © 2016年 Azzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
